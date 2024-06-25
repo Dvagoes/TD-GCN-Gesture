d638103038154bb246b0f903d508f7332b1eddd4
Dvagoes <g.sheltoneckstein@gmail.com>
2024-06-25 13:45:46 +0100

    additions to test.py

--- test.py
+++ test.py
@@ -3,16 +3,16 @@ import torch as t
 import numpy as np
 import pandas as pd
 
-
-sets = ["active", "over", "part_bwd", "part_fwd", "vary_reg", "vary_rnd", "shift_fwd", "shift_bwd"]
+datasets = ["active", "over", "part_bwd", "part_fwd", "vary_reg", "vary_rnd", "shift_fwd", "shift_bwd"]
 data = pd.DataFrame()
 model = t.load_model() #something like that
 # Test 1 - Active frames
 
-for set in sets:
+for ds in datasets:
+    root = "data/DHG14-28/gen_dhgdataset_" + ds + ".py"
     results = [] # store all results
     sequences = [] # get all test sequences in set
-    labels = []
+    labels = [] # 
     for label,sequence in zip(labels,sequences):
         out = model.run(sequence)
         res = (out == label)
