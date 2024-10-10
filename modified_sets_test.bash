fb8097c9e6f6bc9c6f1f2bb475d3fd126e6489c5
Dvagoes <g.sheltoneckstein@gmail.com>
2024-10-10 16:54:47 +0100

    date format

--- modified_sets_test.bash
+++ modified_sets_test.bash
@@ -3,7 +3,7 @@ for test in active over part_fwd part_bwd shift_fwd shift_bwd vary_reg vary_rnd
 do
     python3.9 main.py --config test_${test}.yaml
     #epoch=  53 #$(grep -o -E "Epoch number: [0-9]+" /work_dir/temp/log.txt|grep -o -E '[0-9]+')
-    date=$(date '+%m-%d')
+    date=$(date '+%d-%m')
     mkdir results/test_${test}/${date}
     mv work_dir/dhg14-28/14joint_1/* results/test_${test}/${date}/
     # mv work_dir/temp/runs-${epoch}-* results/test_${test}/${date}
