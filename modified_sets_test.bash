0b2b9aec629cca5026d698c6a7303a796efe75c7
Dvagoes <g.sheltoneckstein@gmail.com>
2024-10-14 12:28:27 +0100

    fixed result paths

--- modified_sets_test.bash
+++ modified_sets_test.bash
@@ -5,12 +5,12 @@ do
     #epoch=  53 #$(grep -o -E "Epoch number: [0-9]+" /work_dir/temp/log.txt|grep -o -E '[0-9]+')
     date=$(date '+%d-%m')
     mkdir results/test_${test}/${date}
-    mv work_dir/dhg14-28/14joint_1/* results/test_${test}/${date}/
+    mv work_dir/dhg14-28/test/14joint_1/* results/test_${test}/${date}/
     # mv work_dir/temp/runs-${epoch}-* results/test_${test}/${date}
     # mv work_dir/temp/epoch${epoch}_test_each_class_acc.csv results/test_${test}/${date}
     # mv work_dir/temp/log.txt results/test_${test}/${date}
-    rm work_dir/temp/*
-    rm work_dir/dhg14-28/14joint_1/*
+    # rm work_dir/temp/*
+    rm work_dir/dhg14-28/test/14joint_1/*
 done
 
 # $ Python3.9 main.py --config test_part_bwd.yaml
